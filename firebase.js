d35700919e6622fa06dab917e27a1c5410d6d7c5
Crystal Martinez <crystalmartinez2028@gmail.com>
2025-06-08 19:14:51 -0500

    update firebase code

--- firebase.js
+++ firebase.js
@@ -1,19 +1,19 @@
+// firebase.js
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';  
 
 const firebaseConfig = {
   apiKey: "AIzaSyCaw62IDbzJyr9CH5a89r5EuinP8qA2wMo",
   authDomain: "loadconnect-ae194.firebaseapp.com",
   projectId: "loadconnect-ae194",
-  storageBucket: "loadconnect-ae194.appspot.com",
+  storageBucket: "loadconnect-ae194.firebasestorage.app",
   messagingSenderId: "91043629998",
   appId: "1:91043629998:web:74417a43991db3ec9d963f"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { db, auth };
-
+export { auth, db };
